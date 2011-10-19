f503f248ebea28b511d2109fb97dd8c546930716
Gonzalo Rodriguez <gonzalo@wyeworks.com>
2011-10-19 00:02:37 -0200

    Minor aspect_listings cleanup

--- app/helpers/aspect_global_helper.rb
+++ app/helpers/aspect_global_helper.rb
@@ -35,7 +35,7 @@ module AspectGlobalHelper
     str = "<span class='aspect_badge single'>"
     link = opts.delete(:link)
     if !link
-      str << link_to(aspect.name, "#", 'data-guid' => aspect.id, :class => 'hard_aspect_link').html_safe
+      str << link_to(aspect.name, "#", 'data-guid' => aspect.id).html_safe
     else
       str << link_for_aspect(aspect).html_safe
     end
@@ -46,7 +46,7 @@ module AspectGlobalHelper
     str = ""
     aspects.each do |aspect|
       str << '<li>'
-      str << link_for_aspect(aspect, :params => opts, 'data-guid' => aspect.id, :class => 'hard_aspect_link').html_safe
+      str << link_for_aspect(aspect, :params => opts, 'data-guid' => aspect.id).html_safe
       str << '</li>'
     end
     str.html_safe
@@ -56,8 +56,6 @@ module AspectGlobalHelper
     opts[:params] ||= {}
     params ||= {}
     opts[:params] = opts[:params].merge("a_ids[]" => aspect.id, :created_at => params[:created_at])
-    opts[:class] ||= ""
-    opts[:class] << " hard_aspect_link"
     opts['data-guid'] = aspect.id
 
     link_to aspect.name, aspects_path( opts[:params] ), opts
