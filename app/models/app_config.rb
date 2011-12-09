076a5da3a17ae31e9dbd869d7a3d642f6dbc6268
Pistos <github.pistos@purepistos.net>
2011-12-09 16:34:54 -0500

    Move normalize_redis_urls above the load! call.

--- app/models/app_config.rb
+++ app/models/app_config.rb
@@ -114,6 +114,15 @@ HELP
     end
   end
 
+  def self.normalize_redis_urls
+    self[:redis_url] = "redis://localhost:6379" if self[:redis_url].blank?
+    self[:redis_cache_url] = self[:redis_url] if self[:redis_cache_url].blank?
+    self[:redis_uri] ||= URI.parse(self[:redis_url])
+    self[:redis_uri].port ||= 6379
+    self[:redis_cache_uri] ||= URI.parse(self[:redis_cache_url])
+    self[:redis_cache_uri].port ||= 6379
+  end
+
   load!
 
   def self.[] (key)
@@ -147,12 +156,4 @@ HELP
     (ENV['SINGLE_PROCESS'] == "true" || ENV['SINGLE_PROCESS_MODE'] == "true" || self[:single_process_mode]) ? true : false
   end
 
-  def self.normalize_redis_urls
-    self[:redis_url] = "redis://localhost:6379" if self[:redis_url].blank?
-    self[:redis_cache_url] = self[:redis_url] if self[:redis_cache_url].blank?
-    self[:redis_uri] ||= URI.parse(self[:redis_url])
-    self[:redis_uri].port ||= 6379
-    self[:redis_cache_uri] ||= URI.parse(self[:redis_cache_url])
-    self[:redis_cache_uri].port ||= 6379
-  end
 end
