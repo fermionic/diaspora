e52130c4d09cc71c4bf83f73234d1d5297bd84ff
Pistos <github.pistos@purepistos.net>
2011-12-27 12:20:35 -0500

    Log at info level, not debug.

--- lib/postzord/receiver/public.rb
+++ lib/postzord/receiver/public.rb
@@ -48,7 +48,7 @@ class Postzord::Receiver::Public < Postzord::Receiver
     begin
       @object.save
     rescue ActiveRecord::RecordNotUnique
-      Rails.logger.debug "Received object (#{@object.class} ID #{@object.id}) already in local DB."
+      Rails.logger.info "Received object (#{@object.class} ID #{@object.id}) already in local DB."
       nil
     end
   end
