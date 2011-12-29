470a92dbc346b33341360e70f3e6b2514332c6e9
Pistos <github.pistos@purepistos.net>
2011-12-28 23:32:57 -0500

    Gracefully handle failure to save comment_or_like before dispatching.

--- lib/diaspora/relayable.rb
+++ lib/diaspora/relayable.rb
@@ -15,7 +15,7 @@ module Diaspora
 
         validates_associated :parent
         validates :author, :presence => true
-        
+
         delegate :public?, :to => :parent
       end
     end
@@ -63,14 +63,17 @@ module Diaspora
 
       #dispatch object DOWNSTREAM, received it via UPSTREAM
       unless user.owns?(comment_or_like)
-        comment_or_like.save!
-        Postzord::Dispatcher.build(user, comment_or_like).post
+        if comment_or_like.save
+          Postzord::Dispatcher.build(user, comment_or_like).post
+        else
+          Rails.logger.info "Failed to save #{comment_or_like.class}: #{comment_or_like.errors.inspect}"
+        end
       end
 
       comment_or_like.socket_to_user(user) if comment_or_like.respond_to? :socket_to_user
 
       if comment_or_like.after_receive(user, person)
-        comment_or_like 
+        comment_or_like
       end
     end
 
