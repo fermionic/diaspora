5a3488fde4cca839e9af7eaee82b5519283a6742
Pistos <github.pistos@purepistos.net>
2012-01-15 23:26:30 -0500

    Bug fix: :additional_subscribers option is supposed to be an Array.

--- app/controllers/reshares_controller.rb
+++ app/controllers/reshares_controller.rb
@@ -6,7 +6,7 @@ class ResharesController < ApplicationController
     @reshare = current_user.build_post(:reshare, :root_guid => params[:root_guid])
     if @reshare.save
       current_user.add_to_streams(@reshare, current_user.aspects)
-      current_user.dispatch_post(@reshare, :url => post_url(@reshare), :additional_subscribers => @reshare.root.author)
+      current_user.dispatch_post(@reshare, :url => post_url(@reshare), :additional_subscribers => [@reshare.root.author])
     end
 
     respond_with @reshare
