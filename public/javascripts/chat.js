af86ef8f4fc79fbd026f3feda1bc920181dbf414
Pistos <github.pistos@purepistos.net>
2012-02-08 00:36:26 -0500

    Click any contact on the Contacts page to begin chatting.

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -102,7 +102,7 @@ $(document).ready( function() {
     }
   } );
 
-  $('#people_stream.contacts .online .content').live( 'click', function() {
+  $('#people_stream.contacts .content').live( 'click', function() {
     showChatMessages();
     var person_id = $(this).data('person_id');
     if( $('.partners .partner[data-person_id="'+person_id+'"]').length ) {
